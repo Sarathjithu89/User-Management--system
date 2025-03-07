d3ab37b29cd1131949c57fee4c9a37b6c54fc06c
Sarath A <sarathjithu89@gmail.com>
2025-03-07 23:09:38 +0530

    Added the Attals

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const app = express();
 const userRoute = require("./routes/userRoutes"); //requiring the user_routes
 const adminRoute = require("./routes/adminRoute"); // Admin routes
 const mongoose = require("mongoose");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
+const URI = process.env.MONGO_URI;
+
+// mongoose
 mongoose
-  .connect("mongodb://127.0.0.1:27017/User_mangement", {
+  .connect(URI, {
     //useNewUrlParser: true,
     //useUnifiedTopology: true,
   })
@@ -16,6 +20,34 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+//DataBase
+
+//Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+// const client = new MongoClient(URI, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log(
+//       "Pinged your deployment. You successfully connected to MongoDB!"
+//     );
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 //user routes
 app.use("/", userRoute);
